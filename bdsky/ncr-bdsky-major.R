f884239255dd9dc5342d6677f617e478d0d1646f
Imari Joy Borda <imari.borda2018@gmail.com>
2022-04-16 19:48:57 +0800

    Minor change

--- bdsky/ncr-bdsky-major.R
+++ bdsky/ncr-bdsky-major.R
@@ -131,8 +131,6 @@ maj <- ggplot(data=df.maj) +
   annotate("pointrange", y = 0.0030, x = as.Date(median(df.pos.maj)), xmin = as.Date(quantile((df.pos.maj), 0.05)), xmax = as.Date(quantile((df.pos.maj), 0.95)),
            colour = "green", size = .5) 
 
-#all <- plot_grid(p, maj, ncol=2,align="v")
-
 # Save Plot
 ggsave(plot = p,
        filename = "ncr-bdsky-major-re.png",
